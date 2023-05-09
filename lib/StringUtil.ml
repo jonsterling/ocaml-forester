ec7b8fcbb23a4bf3ae5013e6110e62e9f6f0e45d
Jon Sterling <jon@jonmsterling.com>
2023-05-09 09:35:07 +0200

    fix titlecasing for first word

--- lib/StringUtil.ml
+++ lib/StringUtil.ml
@@ -1,9 +1,9 @@
-let title_case_word word = 
-  if String.length word > 3 then 
+let title_case_word ix word = 
+  if ix == 0 || String.length word > 3 then 
     String.capitalize_ascii word
   else
     word
 
 let title_case str =
   let words = String.split_on_char ' ' str in
-  String.concat " " @@ List.map title_case_word words
+  String.concat " " @@ List.mapi title_case_word words
