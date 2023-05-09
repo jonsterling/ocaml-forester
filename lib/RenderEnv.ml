04aab8c56f97a335cb891e9cc16b2a942f13c3aa
Jonathan Sterling <jon@jonmsterling.com>
2023-05-09 12:44:48 +0200

    add missing file (how did I not commit this?)

---
+++ lib/RenderEnv.ml
@@ -0,0 +1,11 @@
+open Types
+
+class type t = 
+  object
+    method route : addr -> string
+    method get_doc : addr -> Sem.doc
+    method get_backlinks : addr -> Sem.doc list 
+    method get_links : addr -> Sem.doc list 
+    method get_parents : addr -> Sem.doc list
+    method enqueue_svg : name:string -> source:string -> unit
+  end
