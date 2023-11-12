1d1f8721e07f9ee84acfc1367d21f7e415946b3e
Jon Sterling <jon@jonmsterling.com>
2023-11-12 11:34:43 +0000

    Env: pretty printing for debugging

--- lib/core/Env.ml
+++ lib/core/Env.ml
@@ -2,9 +2,9 @@ include Map.Make (Symbol)
 
 let pp (pp_el : Format.formatter -> 'a -> unit) : Format.formatter -> 'a t -> unit =
   fun fmt map ->
-  Format.fprintf fmt "@[{";
+  Format.fprintf fmt "@[<v1>{";
   begin
     map |> iter @@ fun k v ->
-    Format.fprintf fmt "@[%a ~> %a@;@]" Symbol.pp k pp_el v
+    Format.fprintf fmt "@[%a ~> %a@]@;" Symbol.pp k pp_el v
   end;
   Format.fprintf fmt "}@]"
