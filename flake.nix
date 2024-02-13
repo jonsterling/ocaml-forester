f96d8226e3c7e10706768bf9f91529092dd5c8ca
Kento Okura <kentookura@gmail.com>
2024-02-13 09:28:49 +0000

    Add nix development environment
    Only add lsp, not ocamlformat.

--- flake.nix
+++ flake.nix
@@ -3,20 +3,36 @@
     opam-nix.url = "github:tweag/opam-nix";
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.follows = "opam-nix/nixpkgs";
-    opam-repository = {
-      url = "github:ocaml/opam-repository";
-      flake = false;
-    };
   };
-  outputs = { self, flake-utils, opam-nix, nixpkgs, opam-repository }@inputs:
-
-    flake-utils.lib.eachDefaultSystem (system: {
-      legacyPackages = let
-        inherit (opam-nix.lib.${system}) buildOpamProject;
-        scope =
-          buildOpamProject { repos = [ "${opam-repository}" ]; } "forester" ./.
-          { };
-      in scope;
-      packages.default = self.legacyPackages.${system}."forester";
-    });
+  outputs = { self, flake-utils, opam-nix, nixpkgs }@inputs:
+    let package = "forester";
+    in flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        on = opam-nix.lib.${system};
+        devPackagesQuery = {
+          ocaml-base-compiler = "5.1.1";
+          ocaml-lsp-server = "*";
+        };
+        query = devPackagesQuery // { };
+        scope = on.buildOpamProject' { } ./. query;
+        overlay = final: prev: {
+          ${package} = prev.${package}.overrideAttrs (_: {
+            doNixSupport = false;
+          });
+        };
+        scope' = scope.overrideScope' overlay;
+        main = scope'.${package};
+        devPackages = builtins.attrValues
+          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
+      in {
+        legacyPackages = scope';
+        packages.default = main;
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ main ];
+          buildInputs = devPackages ++ [
+            pkgs.texlive.combined.scheme-full
+          ];
+        };
+      });
 }
